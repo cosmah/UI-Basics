8dff03ea985b71b1df6906b04e756282b02e6836
cosmah <ssekirya.cosma@student.utamu.ac.ug>
2023-09-04 12:21:06 +0300

    added another button and implemented a toast

--- app/src/main/java/com/cosmah/uibasics/MainActivity.java
+++ app/src/main/java/com/cosmah/uibasics/MainActivity.java
@@ -3,14 +3,29 @@ package com.cosmah.uibasics;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    //implement onclick method
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnHello) {
+            Toast.makeText(this, "Hi I'm Cosmah", Toast.LENGTH_SHORT).show();
+            System.out.println("Hi I'm Cosmah");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //button listener
+        Button btnHello = findViewById(R.id.btnHello);
+        btnHello.setOnClickListener(this);
     }
 
     //onclick
@@ -18,4 +33,7 @@ public class MainActivity extends AppCompatActivity {
         TextView txtWelcome = findViewById(R.id.txtWelcome);
         txtWelcome.setText("Hello Again");
     }
+
+
+
 }
