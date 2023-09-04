3991e2f04c10b8e3cfaa41b78cbc70ffc098b6d2
cosmah <ssekirya.cosma@student.utamu.ac.ug>
2023-09-04 13:27:58 +0300

    worked text I/O logic

--- app/src/main/java/com/cosmah/uibasics/MainActivity.java
+++ app/src/main/java/com/cosmah/uibasics/MainActivity.java
@@ -5,16 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //declare as a field
+    private TextView TextHere;
+    private EditText edtxtName;
     //implement onclick method
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.btnHello) {
             Toast.makeText(this, "Hi I'm Cosmah", Toast.LENGTH_SHORT).show();
-            System.out.println("Hi I'm Cosmah");
+            TextHere.setText("Hello " + edtxtName.getText().toString());
+
         }
     }
 
@@ -35,6 +41,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 return true;
             }
         });
+
+        //edit and view
+        edtxtName = findViewById(R.id.edtxtName);
+
+        TextHere = findViewById(R.id.TextHere);
     }
 
     //onclick
